e53ce8f8ca97c08148e171f1c47422f9ae7fbe7a
katiehoesley <katiehoesley@gmail.com>
2021-06-23 13:36:13 -0600

    starting point

--- src/server.js
+++ src/server.js
@@ -3,23 +3,13 @@ const path = require('path');
 const app = express();
 app.use(express.static(path.join(__dirname, 'build')));
 
-const port = 8080
+const PORT = 8080
 app.get('/user', (req, res) => {
     res.status(200).json({ name: "Katie" })
 })
-  
-app.get('/marco', (req, res) => {
-    return res.send('POLO!')
-})
 
-app.get('/ping', (req, res) => {
-    return res.send('pong!')
-})
 
-app.post('/users', (req, res) => {
-    res.send({ name: "john" })
-})
-
-const server = app.listen(port);
-
+const server = app.listen(PORT);
 module.exports = server; 
+
+//or module.exports = app.listen(PORT)
